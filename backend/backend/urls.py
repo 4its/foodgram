9c8ce914efbe4970fda6b66e45a2e8d61838979d
Goerge Egiazaryan <egiazaryan@gmail.com>
2025-01-21 15:37:30 +0300

    fix redirect v.4.2;

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import short_url
+from api.views import RecipeViewSet, short_url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('recipes/<int:pk>/', RecipeViewSet.as_view({'get': 'retrieve'})),
     path('s/<int:pk>/', short_url, name='short_url'),
 ]
 
