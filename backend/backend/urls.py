c33a8de8f540ab5d04635322b0be60703a34c610
Goerge Egiazaryan <egiazaryan@gmail.com>
2025-01-22 09:14:53 +0300

    fix redirect final try(flake8);

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import short_url
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
